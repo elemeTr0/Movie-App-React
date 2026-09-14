4e613caf43b4b0a8e79e911546f73658df53c317
Mateo <141548968+elemeTr0@users.noreply.github.com>
2026-09-14 05:06:13 +0200

    Set basename for BrowserRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Movie-App-React">
       <App />
     </BrowserRouter>
   </StrictMode>,
